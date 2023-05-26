b463e09db7016afeb1944bd3121506e005f5a924
tooi <tooi@stansystem.co.jp>
2023-05-26 16:41:40 +0900

    ooi_update

---
+++ src/test/java/com/example/demo/IaTeam2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IaTeam2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
